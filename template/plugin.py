501f943fd99b537241a9cc8e176345407dcde1a3
edayot <pro.e.dayot@gmail.com>
2024-07-16 18:59:01 +0200

    example

--- template/plugin.py
+++ template/plugin.py
@@ -1,9 +1,16 @@
 from beet import Context
-from simple_item_plugin import NAMESPACE
-
+from simple_item_plugin.types import NAMESPACE, Lang
+from simple_item_plugin.item import Item
 
 
 def beet_default(ctx: Context):
     NAMESPACE.set(ctx.project_id)
     print("Hello, world!")
-
+    graplin = Item(
+        id="graplin",
+        item_name=(
+            f"{NAMESPACE}.item.graplin",
+            {Lang.en_us: "Graplin", Lang.fr_fr: "Graplin"},
+        ),
+        custom_model_data=1432006,
+    )
